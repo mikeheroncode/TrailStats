478003934e7cdba58463a9f2ecc5817d854d2c55
Michael Heron <40605443+mikeheroncode@users.noreply.github.com>
2021-01-26 23:54:11 -0500

    Added hook to access FoodItems

---
+++ src/hooks/useFoodItems.ts
@@ -0,0 +1,45 @@
+import { useState, useEffect } from 'react';
+import { FoodItem } from '../types/FoodItem';
+import { useDatabase } from '../context/DatabaseContext';
+
+// Hook for managing and accessing fooditems (CRUD)
+export function useFoodItems() {
+  const [foodItems, setFoodItems] = useState<FoodItem[]>([]);
+  const [selectedFoodItem, setSelectedFoodItem] = useState<FoodItem>();
+  const database = useDatabase();
+
+  useEffect(() => {
+    refreshListOfFoodItems();
+  }, []);
+
+  function refreshListOfFoodItems() {
+    // Query all fooditems from the DB, then store them as state
+    return database.getAllFoodItems().then(setFoodItems);
+  }
+
+  function addFoodItem(foodItem: FoodItem): Promise<void> {
+    return database.addFoodItem(foodItem).then(refreshListOfFoodItems);
+  }
+
+  function deleteFoodItem(foodItemToDelete: FoodItem): Promise<void> {
+    if (foodItemToDelete !== undefined) {
+      return database
+        .deleteFoodItem(foodItemToDelete)
+        .then(refreshListOfFoodItems);
+    }
+    // otherwise:
+    return Promise.reject(Error('Could not delete an undefined foodItem'));
+  }
+
+  async function selectFoodItem(foodItem: FoodItem) {
+    setSelectedFoodItem(foodItem);
+  }
+
+  return {
+    foodItems,
+    selectFoodItem,
+    addFoodItem,
+    deleteFoodItem,
+    selectedFoodItem,
+  };
+}
